ac02cb424c98ae0b2dc78d6accec3c03aca09bcc
aldusosi <desenho2510@gmail.com>
2021-08-31 11:36:13 -0300

    rota-criar-documento-finalizada

--- src/controladores/criarDocumento.js
+++ src/controladores/criarDocumento.js
@@ -1,8 +1,34 @@
 const knex = require("../conexao/conexao");
+const bcrypt = require("bcrypt");
+const memorySizeOf = require("../utilities/VerificaTamanho");
 
 async function criarDocumento(req, res){
-    const documents = await knex('documents');
-    res.json(documents);
+    const { nome, content } = req.body;
+    const kbsizen = memorySizeOf(content);
+
+    if(!nome || !content){
+        return res.status(400).json('Todos os campos são obrigatórios.')
+    }
+
+    try {
+        const data = new Date();
+        const newDoc = {
+            kbsizen: kbsizen,
+            nome: nome,
+            content: content,
+            criatedat: data
+        }
+
+        const documentos = await knex('documents').insert(newDoc);
+
+        if(documentos.length === 0){
+            return res.status(400).json('Erro ao cadastrar documento')
+        }
+
+        res.json('sucesso ao cadastrar.');
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
     
 }
 
